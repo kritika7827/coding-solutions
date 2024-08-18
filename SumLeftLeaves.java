8a8cdac73f01b1f90a50921eb24e8f4839a72832
kritika7827 <h20171030071h@alumni.bits-pilani.ac.in>
2024-08-18 16:45:34 +0530

    Create SumLeftLeaves.java

---
+++ SumLeftLeaves.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int sumOfLeftLeaves(TreeNode root) {
+
+        return dfs(root, false);
+
+    }
+
+    public int dfs(TreeNode subtree, boolean isLeft){
+
+        if(subtree == null) return 0;
+
+        if(subtree.left == null && subtree.right == null) {
+            return isLeft ? subtree.val : 0;
+        }
+
+        return dfs(subtree.left, true) +dfs(subtree.right, false);
+
+    }
+}
